e81b1e2b83569f43e18ff256844611faf546b899
vignedev <vignedev@gmail.com>
2025-01-31 03:33:12 +0100

    SDLBackend: prevent meta-key from being passed below

--- src/Backends/SDLBackend.cpp
+++ src/Backends/SDLBackend.cpp
@@ -743,6 +743,11 @@ namespace gamescope
 				{
 					uint32_t key = SDLScancodeToLinuxKey( event.key.keysym.scancode );
 
+					if ( key == KEY_LEFTMETA || key == KEY_RIGHTMETA )
+					{
+						break;
+					}
+
 					if ( event.type == SDL_KEYUP && ( event.key.keysym.mod & KMOD_LGUI ) )
 					{
 						bool handled = true;
